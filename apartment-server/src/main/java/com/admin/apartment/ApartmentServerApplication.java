710d051cf9171aaea5b7acdd0de946e9c2fbea56
梁明 <35681222+liangming011@users.noreply.github.com>
2019-09-24 09:55:13 +0800

    正式页面生成

--- apartment-server/src/main/java/com/admin/apartment/ApartmentServerApplication.java
+++ apartment-server/src/main/java/com/admin/apartment/ApartmentServerApplication.java
@@ -1,20 +1,36 @@
 package com.admin.apartment;
+import	java.security.acl.Group;
 
 
+import com.admin.apartment.common.CommonResult;
+import com.admin.apartment.entity.UmsAdmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableScheduling
+@RestController
 public class ApartmentServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApartmentServerApplication.class, args);
     }
 
+    /**
+     * 登录
+     * */
+//    @GetMapping("/")
+//    public @ResponseBody
+//    ModelAndView login(){
+//        ModelAndView modelAndView= new ModelAndView();
+//        modelAndView.setViewName("forward:/login");
+//       return modelAndView;
+//    }
 }
